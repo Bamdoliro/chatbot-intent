ef94ab42dca2f7f25dc5b6a1c317f090f483d554
YuLim2 <jyulim0120@gmail.com>
2022-07-03 23:06:00 +0900

    FIX: fixed untracked files

--- config/DatabaseConfig.py
+++
@@ -1,7 +0,0 @@
-DB_HOST = "127.0.0.1"
-DB_USER = "homestead"
-DB_PASSWORD = "12test34"
-DB_NAME = "gati"
-
-def DatabaseConfig():
-    global DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
--- config/GlobalParams.py
+++
@@ -1,6 +0,0 @@
-# 단어 시퀀스 벡터 크기
-MAX_SEQ_LEN = 15
-
-
-def GlobalParams():
-    global MAX_SEQ_LEN
