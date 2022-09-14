3bf211b9b7596ab662e8942d13304ddc100960bd
YuLim2 <jyulimmm@gmail.com>
2022-09-14 19:32:51 +0900

    ADD: chatbot test with database

--- test/chatbot_test.py
+++ test/chatbot_test.py
@@ -5,12 +5,14 @@ from utils.Preprocess import Preprocess
 # 전처리 객체 생성
 p = Preprocess(word2index_dic='../train_tools/dict/chatbot_dict.bin',
                userdic='../utils/user_dic.tsv')
+print(1)
 
 # 질문/답변 학습 DB 연결 객체 생성
 db = Database(
     host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db_name=DB_NAME
 )
 db.connect()  # DB 연결
+print(1)
 
 # 원문
 query = input()
@@ -21,11 +23,15 @@ intent = IntentModel(model_name='../models/intent/intent_model.h5', preprocess=p
 predict = intent.predict_class(query)
 intent_name = intent.labels[predict]
 
+print(1)
+
 # 개체명 인식
 from models.ner.NerModel import NerModel
 ner = NerModel(model_name='../models/ner/ner_model.h5', preprocess=p)
 predicts = ner.predict(query)
 ner_tags = ner.predict_tags(query)
+
+print(1)
 
 print("질문 : ", query)
 print("=" * 40)
